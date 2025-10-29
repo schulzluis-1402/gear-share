02f7c8b9fc54e26704ce573e8fc0a14a1f5ea840
schulzluis-1402 <schulzluis@icloud.com>
2025-10-29 10:46:48 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+const items = [
+  { name: "Mountainbike", price: "5€/Tag", location: "Innsbruck Zentrum" },
+  { name: "Ski Set", price: "10€/Tag", location: "Höttinger Au" }
+];
+
+const list = document.getElementById("list");
+
+items.forEach(item => {
+  const el = document.createElement("div");
+  el.innerHTML = `<strong>${item.name}</strong><br>${item.price}<br>${item.location}`;
+  el.className = "item";
+  list.appendChild(el);
+});
 
